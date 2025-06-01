a79673a20bf62737381330570741bc3310194f20
Reimolaev <149591499+Sek13H@users.noreply.github.com>
2025-06-01 19:28:11 +0500

    Add files via upload

---
+++ FamiRom/AboutForm.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace FamiRom
+{
+    partial class AboutForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AboutForm));
+            this.PictureBox = new System.Windows.Forms.PictureBox();
+            this.AboutLabel1 = new System.Windows.Forms.Label();
+            this.AboutLabel2 = new System.Windows.Forms.Label();
+            this.AboutLabel3 = new System.Windows.Forms.Label();
+            this.AboutLabel4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PictureBox
+            // 
+            this.PictureBox.Image = global::FamiRom.Properties.Resources.PictureBox;
+            this.PictureBox.Location = new System.Drawing.Point(0, 1);
+            this.PictureBox.Name = "PictureBox";
+            this.PictureBox.Size = new System.Drawing.Size(216, 669);
+            this.PictureBox.TabIndex = 0;
+            this.PictureBox.TabStop = false;
+            // 
+            // AboutLabel1
+            // 
+            this.AboutLabel1.AutoSize = true;
+            this.AboutLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.AboutLabel1.Location = new System.Drawing.Point(230, 13);
+            this.AboutLabel1.Name = "AboutLabel1";
+            this.AboutLabel1.Size = new System.Drawing.Size(212, 25);
+            this.AboutLabel1.TabIndex = 1;
+            this.AboutLabel1.Text = "FamiRom By reimolaev";
+            // 
+            // AboutLabel2
+            // 
+            this.AboutLabel2.AutoSize = true;
+            this.AboutLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.AboutLabel2.Location = new System.Drawing.Point(230, 331);
+            this.AboutLabel2.Name = "AboutLabel2";
+            this.AboutLabel2.Size = new System.Drawing.Size(117, 25);
+            this.AboutLabel2.TabIndex = 2;
+            this.AboutLabel2.Text = "Version: 1.0";
+            // 
+            // AboutLabel3
+            // 
+            this.AboutLabel3.AutoSize = true;
+            this.AboutLabel3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.AboutLabel3.Location = new System.Drawing.Point(227, 99);
+            this.AboutLabel3.Name = "AboutLabel3";
+            this.AboutLabel3.Size = new System.Drawing.Size(369, 90);
+            this.AboutLabel3.TabIndex = 3;
+            this.AboutLabel3.Text = "Thanks:\r\nNesDev.org: Mappers Name\r\nnesdir.github.io: For the names of games with " +
+    "mappers\r\n@Malaert64: Icon\r\n\r\n";
+            // 
+            // AboutLabel4
+            // 
+            this.AboutLabel4.AutoSize = true;
+            this.AboutLabel4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.AboutLabel4.Location = new System.Drawing.Point(232, 374);
+            this.AboutLabel4.Name = "AboutLabel4";
+            this.AboutLabel4.Size = new System.Drawing.Size(113, 17);
+            this.AboutLabel4.TabIndex = 4;
+            this.AboutLabel4.Text = "reimolaev, 2025.";
+            // 
+            // AboutForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(595, 400);
+            this.Controls.Add(this.AboutLabel4);
+            this.Controls.Add(this.AboutLabel3);
+            this.Controls.Add(this.AboutLabel2);
+            this.Controls.Add(this.AboutLabel1);
+            this.Controls.Add(this.PictureBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "AboutForm";
+            this.Text = "AboutForm";
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox PictureBox;
+        private System.Windows.Forms.Label AboutLabel1;
+        private System.Windows.Forms.Label AboutLabel2;
+        private System.Windows.Forms.Label AboutLabel3;
+        private System.Windows.Forms.Label AboutLabel4;
+    }
+}
---
+++ FamiRom/MainForm.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace FamiRom
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.MenuStrip = new System.Windows.Forms.MenuStrip();
+            this.AboutStrip = new System.Windows.Forms.ToolStripMenuItem();
+            this.AboutToolStrip = new System.Windows.Forms.ToolStripMenuItem();
+            this.GitHubStrip = new System.Windows.Forms.ToolStripMenuItem();
+            this.NesDevStrip = new System.Windows.Forms.ToolStripMenuItem();
+            this.IconStrip = new System.Windows.Forms.ToolStripMenuItem();
+            this.Selectbtn = new System.Windows.Forms.Button();
+            this.OutputLabel = new System.Windows.Forms.Label();
+            this.MenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // MenuStrip
+            // 
+            this.MenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.AboutStrip});
+            this.MenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.MenuStrip.Name = "MenuStrip";
+            this.MenuStrip.Size = new System.Drawing.Size(800, 24);
+            this.MenuStrip.TabIndex = 0;
+            this.MenuStrip.Text = "menuStrip1";
+            // 
+            // AboutStrip
+            // 
+            this.AboutStrip.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.AboutToolStrip,
+            this.GitHubStrip,
+            this.NesDevStrip,
+            this.IconStrip});
+            this.AboutStrip.Name = "AboutStrip";
+            this.AboutStrip.Size = new System.Drawing.Size(82, 20);
+            this.AboutStrip.Text = "About/Help";
+            // 
+            // AboutToolStrip
+            // 
+            this.AboutToolStrip.Name = "AboutToolStrip";
+            this.AboutToolStrip.Size = new System.Drawing.Size(180, 22);
+            this.AboutToolStrip.Text = "About";
+            this.AboutToolStrip.Click += new System.EventHandler(this.AboutToolStrip_Click);
+            // 
+            // GitHubStrip
+            // 
+            this.GitHubStrip.Name = "GitHubStrip";
+            this.GitHubStrip.Size = new System.Drawing.Size(180, 22);
+            this.GitHubStrip.Text = "GitHub";
+            this.GitHubStrip.Click += new System.EventHandler(this.GitHubStrip_Click);
+            // 
+            // NesDevStrip
+            // 
+            this.NesDevStrip.Name = "NesDevStrip";
+            this.NesDevStrip.Size = new System.Drawing.Size(180, 22);
+            this.NesDevStrip.Text = "NesDev.org";
+            this.NesDevStrip.Click += new System.EventHandler(this.NesDevStrip_Click);
+            // 
+            // IconStrip
+            // 
+            this.IconStrip.Name = "IconStrip";
+            this.IconStrip.Size = new System.Drawing.Size(180, 22);
+            this.IconStrip.Text = "Icon";
+            this.IconStrip.Click += new System.EventHandler(this.IconStrip_Click);
+            // 
+            // Selectbtn
+            // 
+            this.Selectbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.Selectbtn.Location = new System.Drawing.Point(10, 45);
+            this.Selectbtn.Name = "Selectbtn";
+            this.Selectbtn.Size = new System.Drawing.Size(779, 106);
+            this.Selectbtn.TabIndex = 1;
+            this.Selectbtn.Text = "Select .nes File";
+            this.Selectbtn.UseVisualStyleBackColor = true;
+            this.Selectbtn.Click += new System.EventHandler(this.Selectbtn_Click);
+            // 
+            // OutputLabel
+            // 
+            this.OutputLabel.AutoSize = true;
+            this.OutputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.OutputLabel.Location = new System.Drawing.Point(273, 192);
+            this.OutputLabel.Name = "OutputLabel";
+            this.OutputLabel.Size = new System.Drawing.Size(108, 25);
+            this.OutputLabel.TabIndex = 2;
+            this.OutputLabel.Text = "Information";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 631);
+            this.Controls.Add(this.OutputLabel);
+            this.Controls.Add(this.Selectbtn);
+            this.Controls.Add(this.MenuStrip);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.MenuStrip;
+            this.Name = "MainForm";
+            this.Text = "FamiRom";
+            this.MenuStrip.ResumeLayout(false);
+            this.MenuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip MenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem AboutStrip;
+        private System.Windows.Forms.Button Selectbtn;
+        private System.Windows.Forms.Label OutputLabel;
+        private System.Windows.Forms.ToolStripMenuItem AboutToolStrip;
+        private System.Windows.Forms.ToolStripMenuItem GitHubStrip;
+        private System.Windows.Forms.ToolStripMenuItem NesDevStrip;
+        private System.Windows.Forms.ToolStripMenuItem IconStrip;
+    }
+}
+
